3cf86a984f3805032113949a71aff9b0c144c858
ElenaSukhomlinova <fandeevaf2@gmail.com>
2025-01-12 21:20:24 +0300

    Homework done

--- src/org/skypro/skyshop/product/Product.java
+++ src/org/skypro/skyshop/product/Product.java
@@ -1,25 +1,25 @@
 package org.skypro.skyshop.product;
 
-public class Product {
+public abstract class Product {
 
     private String productName;
-    private int productCost;
 
-    public Product(String productName, int productCost) {
-        if (productCost <= 0) {
-            throw new IllegalArgumentException("Стоимость должна быть больше 0");
-        }
+
+    public Product(String productName) {
+
         this.productName = productName;
-        this.productCost = productCost;
+
     }
 
     public String getProductName() {
         return productName;
     }
 
-    public int getProductCost() {
-        return productCost;
-    }
+    public abstract int getProductCost();
+
+    public abstract String toString();
+
+    public abstract boolean isSpecial();
 
 
 }
